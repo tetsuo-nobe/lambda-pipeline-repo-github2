bb9ea4ec87e52c4731e686894a28c39b5764437e
tnobe <tnobe@amazon.co.jp>
2020-08-13 11:58:19 +0900

    3rd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date();
-    currentTime.setTimezone("America/Los_Angeles"); ////
+    currentTime.setTimezone("America/Los_Angeles"); 
     callback(null, {
         statusCode: '200',
         body: 'The time in Los Angeles is: ' + currentTime.toString(),
